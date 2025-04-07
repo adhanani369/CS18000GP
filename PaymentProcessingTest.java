41b1957955a80fe417b6366dcaff7753f34f4a82
zeng322 <zeng322@purdue.edu>
2025-04-06 22:08:20 -0400

    PaymentProcessingTest.java
    question with testAddFunds()

--- PaymentProcessingTest.java
+++ PaymentProcessingTest.java
@@ -1,16 +1,26 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import java.util.List;
 import org.junit.Test;
 
 public class PaymentProcessingTest {
+
+    /*
+     * check for funds to a user's account before and after adding
+     *
+     */
     @Test
-    public void testProcessPayment() {
-        Database database = new Database();
-        User buyer = new User("sarah", "123", "hello", database);
-        User seller = new User("papa", "123", "hello", database);
-        Item item = new Item(seller.getUserId(), "dog", "genya", "animal", 30);
-        database.addItem(item);
-        //assertEquals(0, result);
+    public void testAddFunds() {
+        Database db = new Database();
+        PaymentProcessing pp = new PaymentProcessing(db);
+        double initialBalance = 100.0;
+        double amount = 0.0;
+
+        User user = new User("username", "pass", "bio", db);
+        user.depositFunds(initialBalance);  // Set the initial balance to 100.0
+        db.addUser("username", "pass", "bio");
+
+        boolean result = pp.addFunds(user.getUserId(), amount);
+        assertTrue(result);
     }
+
 }
-
-
