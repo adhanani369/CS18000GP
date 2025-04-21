82167726aed08a7f6372e4032f4716e9e519c6f2
rayaangrewalpurdue <143538191+rayaangrewalpurdue@users.noreply.github.com>
2025-04-20 21:08:29 -0400

    Update ItemInterface.java

--- ItemInterface.java
+++ ItemInterface.java
@@ -2,37 +2,19 @@ import java.util.List;
 
 public interface ItemInterface {
     String getItemId();
-
     String getSellerId();
-
     String getTitle();
-
     String getDescription();
-
     String getCategory();
-
     List<String> getTags();
-
     double getPrice();
-
     double getRating();
-
     void updateRating(double newRating);
-
     boolean isSold();
-
     boolean markAsSold(String buyerId);
-
     String getBuyerId();
-
     List<String> getStopwords();
-
     List<String> extractTags(List<String> stopwords);
-
     void getSpecialCharacters();
-
-    /*
-     * Cleans the word from any commas or other special characters
-     */
     String cleanWord(String word);
 }
