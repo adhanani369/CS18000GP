71a7458348492344c9b1bb51779f2aad20ed38f7
zeng322 <zeng322@purdue.edu>
2025-04-20 20:17:16 -0400

    create ServerTest.java

---
+++ ServerTest.java
@@ -0,0 +1,9 @@
+public class ServerTest {
+    static class TestHandler {
+        private Database database;
+
+        public TestHandler(Database db) {
+            this.database = db;
+        }
+    }
+}
