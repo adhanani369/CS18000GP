536762d45dac76dad04e163a1f84265717c53c1a
rayaangrewalpurdue <143538191+rayaangrewalpurdue@users.noreply.github.com>
2025-04-20 21:07:23 -0400

    Update DatabaseInterface.java

--- DatabaseInterface.java
+++ DatabaseInterface.java
@@ -9,38 +9,29 @@ import java.util.Map;
 public interface DatabaseInterface {
     boolean addUser(String username, String password, String bio);
     boolean login(String data);
-  
-    
     User userExists(String username);
     void readUserFile();
-  
     void writeUserFile();
     void writeItemFile();
     void readMessageFiles();
-  
     List<User> getAllUsers();
     boolean addItem(Item item);
-  
     User getUserByUsername(String username);
     User getUserById(String userId);
     Item getItemById(String itemId);
-  
     List<Item> getAllItems();
     List<Item> getActiveItems();
-  
     boolean addMessage(Message message, String itemId);
     boolean addMessage(Message message);
-  
     List<Message> getMessagesBetweenBuyerAndSeller(String buyerId, String sellerId);
     Map<String, List<String>> getUserRoleBasedConversations(String userId);
-  
     List<String> getAllConversationPartners(String userId);
-    
-  
     void writeMessageFile();
-  
     void readItemFile();
-  
     boolean removeItem(String itemId, String requesterId);
     List<Message> getMessagesBetweenUsers(String user1Id, String user2Id);
+    boolean deleteUser(String username);
+    boolean addSellerRating(String sellerId, double rating);
+    double getSellerRating(String sellerId);
+    Map<String, List<Double>> readRatingsFile();
 }
