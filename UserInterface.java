0dd65fd5e3be5e756abd48408ed871507ab53b92
rayaangrewalpurdue <143538191+rayaangrewalpurdue@users.noreply.github.com>
2025-04-20 21:08:57 -0400

    Update UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -2,30 +2,17 @@ import java.util.ArrayList;
 
 public interface UserInterface {
     String getBio();
-
     String getUserId();
-
     String getUsername();
-
     boolean validatePassword(String password);
-
     void depositFunds(double amount);
-
     boolean withdrawFunds(double amount);
-
     double getBalance();
-
     void addListing(Item item);
-
     boolean removeListing(String itemId);
-
     void addToPurchaseHistory(Item item);
-
     void recordItemSold(Item item);
-
     ArrayList<Item> getActiveListings();
-
     ArrayList<Item> getPurchaseHistory();
-
     ArrayList<Item> getSoldItems();
 }
